b88aa9ee7dd5ab1161748f0b3bb2d0c34d607852
kamran <kamranawan1@yahoo.com>
2018-04-27 15:52:55 +0500

    Changes made in landing page

--- test/models/forum_thread_test.rb
+++ test/models/forum_thread_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ForumThreadTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@forumthread = ForumThread.new(user_id: 1 ,first_name: "Abdullah zulifqar", 
+  		           subject: "The latest post for testing the app")
+  end
+
+  test "User name Must be Present in forum post" do 
+  	assert @forumthread.valid?
+  end 
+  # test "User name can not be empty string " do 
+  # 	@forumthread.first_name = " "
+  # 	assert_not @forumthread.valid?
+  # end 
+  test "forum post is containing the subject" do
+  	@forumthread
+  	assert @forumthread.valid?
+  end 
+
+  # test "Subject should pe present with forumthread" do
+  # 	@forumthread.subject = " " 
+  # 	assert_not @forumthread.valid?
+  # end 
 end
